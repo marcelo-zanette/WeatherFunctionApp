9c1d9cc6d49e09ff273b3f34a54a90534207d47a
Marcelo Zanette <marcelo.zanette@natzka.com>
2020-12-15 14:30:33 +0100

    using scale-start and scale-end variables

--- WeatherFunctionApp/Function1.cs
+++ WeatherFunctionApp/Function1.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace WeatherFunctionApp
@@ -15,22 +16,51 @@ namespace WeatherFunctionApp
         [FunctionName("WeatherFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ExecutionContext context,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var environment = config["Environment"];
+
+            int start;
+            int end;
+            try
+            {
+                start = int.Parse(config["scale_start"]);
+                end = int.Parse(config["scale_end"]);
+            }
+            catch
+            {
+                start = 1000;
+                end = 2000;
+            }
 
             string city = req.Query["city"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
+
             city = city ?? data?.name;
 
-            decimal deg = new Random().Next(100, 200) / 10;
+            decimal deg = ((decimal)new Random().Next(start, end) / 10);
             decimal temp = Math.Round(deg, 1);
 
-            string responseMessage = string.IsNullOrEmpty(city)
-                ? "City name not defined (city=[city-name])"
-                : $"The weather in {city} is around {temp} C.";
+            string responseMessage = 
+                environment + 
+                "-" + 
+                (string.IsNullOrEmpty(city)
+                    ? "City name not defined (city=[city-name])"
+                    : $"The weather in {city} is around {temp} C."
+                );
+
+            log.LogInformation(responseMessage);
 
             return new OkObjectResult(responseMessage);
         }
